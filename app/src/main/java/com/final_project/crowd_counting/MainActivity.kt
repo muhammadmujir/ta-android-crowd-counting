5323510eab355e32f529ce4edc6d444fbfd066c3
muhammadmujir <muhammadmujir011@gmail.com>
2022-03-24 21:20:40 +0700

    initial commit

---
+++ app/src/main/java/com/final_project/crowd_counting/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.final_project.crowd_counting
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
